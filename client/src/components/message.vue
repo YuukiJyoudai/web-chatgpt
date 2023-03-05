d69f081fdc74e5fc698d2cd9d829dd9c90df5bf3
YuukiJyoudai <1374341727@qq.com>
2023-03-05 16:10:24 +0800

    feat(tem): tem

--- client/src/components/message.vue
+++ client/src/components/message.vue
@@ -1,32 +1,34 @@
 <script setup lang="ts">
-import {defineProps, watchEffect, ref, onMounted} from 'vue'
+import {defineProps, watchEffect, ref, nextTick, computed} from 'vue'
 
-const prop = defineProps({
+const props = defineProps({
     name: {
-        type: String
+        type: [String, Number]
     },
     value: {
-        type: String
+        type: [String, Number]
     }
 })
+const isUser = computed(() => {
+    return props.name === 1
+})
 const dom = ref(null)
-onMounted(() => {
-    watchEffect(() => {
+watchEffect(() => {
+    nextTick(() => {
         dom.value.scrollIntoView()
     })
 })
 
 </script>
 <template>
-    <li ref="dom" class="flex flex-row mt-4">
-        <div class="flex flex-shrink-0 pt-2 pr-2 items-start">
-            123
+    <li ref="dom" class="flex flex-row mt-4" :class="{
+        'flex-row-reverse': isUser
+    }">
+        <div class="flex flex-shrink-0 items-start">
+            <svg-icon class="w-8 h-8 text-purple-400" :name="isUser ? 'me2' : 'am'"></svg-icon>
         </div>
-        <div class="bg-purple-100 rounded-xl px-4 py-2 flex-1 shadow-sm shadow-purple-200 text-purple-600">
+        <div class="text-xs tracking-wide bg-purple-100 rounded-xl mx-2 px-4 py-2 flex-1 shadow-sm shadow-purple-200 text-purple-600">
             {{ value }}
         </div>
     </li>
 </template>
-
-<style scoped>
-</style>
